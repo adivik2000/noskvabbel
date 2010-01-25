2efc48babcb3df6b203c7a9522b412577c7908a3
unknown <Laetitia@.(none)>
2010-01-25 22:40:01 +0100

    added new but missing file

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  # GET /users
+  # GET /users.xml
+  def index
+  end
+
+  # GET /users/1
+  # GET /users/1.xml
+  def show
+
+  end
+
+  # GET /users/new
+  # GET /users/new.xml
+  def new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  def add_pers
+  end
+
+  def del_pers
+  end
+
+end
