21b11baed413b06a729a85566d7c886f4fef8434
Guillaume Desrat <guillaume.desrat@zlab.fr>
2010-01-31 16:16:52 +0100

    Extended the manage group feature

--- features/step_definitions/group_steps.rb
+++ features/step_definitions/group_steps.rb
@@ -1,9 +1,5 @@
-Given /^I am authenticated$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
 Given /^I belong to groups named (.+)$/ do |names|
   names.split(',').each do |name|
-    Group.create!(:name => name.gsub('"', ''), :users => [User.create!()])
+    Group.create!(:name => name.gsub('"', ''), :users => [User.find_by_username('zifro')])
   end
 end
